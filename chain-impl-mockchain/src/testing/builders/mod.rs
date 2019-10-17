ae675047f283593bb353462edd9408c6cd8a6fda
dkijania <dariusz.kijania@iohk.io>
2019-10-17 13:54:42 +0200

    moved request module into initial_builder modelu (more meaningful) and updated usages

--- chain-impl-mockchain/src/testing/builders/mod.rs
+++ chain-impl-mockchain/src/testing/builders/mod.rs
@@ -6,9 +6,11 @@ pub mod proposal_builder;
 pub mod tx_builder;
 pub mod tx_cert_builder;
 pub mod witness_builder;
+mod initial_builder;
 
 pub use block_builder::*;
 pub use cert_builder::*;
+pub use initial_builder::*;
 pub use stake_pool_builder::*;
 pub use old_address_builder::*;
 pub use proposal_builder::*;pub use tx_builder::*;
